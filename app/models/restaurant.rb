94522014580d5ce3c6c6ddc210208230f9fa9eee
Sebastian Lipiszko <lipsikko@gmail.com>
2019-08-21 15:29:36 +1000

    Rake test past

---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurant < ApplicationRecord
+  VALID_CATEGORIES = %w[chinese italian japanese french belgian]
+
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, inclusion: { in: VALID_CATEGORIES }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Restaurant.create!([{ name: 'Subway',
+                        address: 'Melbourne',
+                        phone_number: "000",
+                        category: 'italian' }])
+end
+
+
